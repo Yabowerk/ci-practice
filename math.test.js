60b3282065515ab8a11e9fb044c2a3b952e71eaa
Yabowerk <abityab34@gmail.com>
2026-09-24 03:18:22 +0300

    add multiply function with bug

--- math.test.js
+++ math.test.js
@@ -1,5 +1,6 @@
-import add from './maths.js';
+import {add,multiply} from './maths.js';
 import assert from "assert";
 
 assert.strictEqual(add(2, 3), 5);
+assert.strictEqual(multiply(2, 3), 6);
 console.log("Test passed!");
--- maths.js
+++ maths.js
@@ -1,4 +1,9 @@
-export default function add(a, b) {
+export  function add(a, b) {
   return a + b;
 }
+export function multiply(a, b) {
+  return a + b;  
+}
+
+
 
